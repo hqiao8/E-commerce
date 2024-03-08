86edf1995b6ba8d2813ef5b70d7d35e5e13ee15c
hqiao8 <qiaohaipeng014@gmail.com>
2024-03-07 22:48:16 -0700

    Work on Header

---
+++ Smartphone/front-end/src/components/Header.js
@@ -0,0 +1,8 @@
+
+const Header = () => (
+    <header className=".bg-info d-flex justify-content-between">
+        <h1>Smartphone</h1>
+    </header>
+)
+
+export default Header
---
+++ Smartphone/front-end/src/pages/Home.js
@@ -0,0 +1,7 @@
+import Header from "../components/Header";
+
+const Home = () => (
+    <Header></Header>
+)
+
+export default Home
