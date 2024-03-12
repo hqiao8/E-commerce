5b3ebe65e4e031c3ab75b259e53227e825f3a7e3
hqiao8 <qiaohaipeng014@gmail.com>
2024-03-12 13:32:54 -0600

    Finish the visual design of Header

--- Smartphone/front-end/src/components/Header.js
+++ Smartphone/front-end/src/components/Header.js
@@ -1,8 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faShoppingCart} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => (
-    <header className=".bg-info d-flex justify-content-between">
-        <h1>Smartphone</h1>
+    <header className="bg-info p-3 clearfix">
+        <span className="float-start p-3 display-5 text-success">
+            SMARTphone
+        </span>
+        <span className="float-end p-2">
+            <button type="button" className="m-2 btn btn-outline-primary rounded-pill">
+                Login
+            </button>
+            <button type="button" className="m-2 btn btn-outline-primary rounded-pill">
+                <FontAwesomeIcon icon={faShoppingCart}/>
+                <span className="ms-2 badge bg-warning p-1">
+                    0
+                </span>
+            </button>
+        </span>
     </header>
-)
+);
 
-export default Header
+export default Header;
