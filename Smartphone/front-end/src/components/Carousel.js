d86a1e697d9d1e1f2b38c8b6826d772c1edad303
hqiao8 <qiaohaipeng014@gmail.com>
2024-03-28 20:53:28 -0600

    Update the Carousel for minor formatting changes

--- Smartphone/front-end/src/components/Carousel.js
+++ Smartphone/front-end/src/components/Carousel.js
@@ -7,9 +7,20 @@ import CarouselControl from "./CarouselControl";
 const Carousel = () => {
     let indicators = [];
     let items = [];
-    const images = [["iphone15Pro","https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-model-unselect-gallery-1-202309?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1693010533609"],
-                    ["iphone15","https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-model-unselect-gallery-1-202309?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1692810665888"],
-                    ["iphone14","https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-14-model-unselect-gallery-1-202209?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660689596976"]];
+    const images = [
+        [
+            "iphone15Pro",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-model-unselect-gallery-1-202309?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1693010533609"
+        ],
+        [
+            "iphone15",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-model-unselect-gallery-1-202309?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1692810665888"
+        ],
+        [
+            "iphone14",
+            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-14-model-unselect-gallery-1-202209?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660689596976"
+        ]
+    ];
     for (let i = 0; i < images.length; i++) {
         indicators.push(<CarouselIndicator id="carousel" number={i.toString()}/>);
         items.push(<CarouselItem source={images[i][1]} alternate={"Carousel" + images[i][0]} number={i.toString()}/>);
