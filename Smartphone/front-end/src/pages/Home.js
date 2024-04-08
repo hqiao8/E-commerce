29adb77e8662dc33f76d2150ddaf1d613a5550c6
hqiao8 <qiaohaipeng014@gmail.com>
2024-04-08 17:41:17 -0600

    Update the Home

--- Smartphone/front-end/src/pages/Home.js
+++ Smartphone/front-end/src/pages/Home.js
@@ -1,11 +1,20 @@
 import Header from "../components/Header";
 import Carousel from "../components/Carousel";
+import Tabs from "../components/Tabs";
 
-const Home = () => (
-    <div>
-        <Header/>
-        <Carousel/>
-    </div>
-);
+const Home = () => {
+    const images = [
+        "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-model-unselect-gallery-1-202309?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1693010533609",
+        "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-model-unselect-gallery-1-202309?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1692810665888",
+        "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-14-model-unselect-gallery-1-202209?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660689596976"
+    ];
+    return (
+        <div>
+            <Header/>
+            <Carousel id="Carousel" images={images}/>
+            <Tabs/>
+        </div>
+    );
+}
 
 export default Home;
