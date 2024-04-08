5c231229ba0ef2f1a637c17ab2ec9040acf05dd2
hqiao8 <qiaohaipeng014@gmail.com>
2024-04-08 17:39:24 -0600

    Update the Card for Extract Variable Refactorings and the Carousel addition

--- Smartphone/front-end/src/components/Card.js
+++ Smartphone/front-end/src/components/Card.js
@@ -1,23 +1,28 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
+import Carousel from "./Carousel";
 
 const Card = props => {
+    const product = props.product;
+    const model = product.model;
+    const id = model.replace(/\s+/g, "-");
     return [
         <figure className="card w-50">
-            <img src={props.product.image} alt={props.product.model} className="card-img-top img-thumbnail w-100"/>
+            <img src={product.image} alt={model} className="card-img-top img-thumbnail w-100"/>
             <figcaption className="card-body">
                 <h6 className="card-title">
-                    {props.product.model}
+                    {model}
                 </h6>
-                <button type="button" data-bs-toggle="modal" data-bs-target={"#" + props.product.model.replace(/\s+/g, "")} className="btn btn-primary">
+                <button type="button" data-bs-toggle="modal" data-bs-target={"#" + id} className="btn btn-primary">
                     Details
                 </button>
             </figcaption>
         </figure>,
-        <section className="modal fade" id={props.product.model.replace(/\s+/g, "")}>
+        <section className="modal fade" id={id}>
             <section className="modal-dialog modal-fullscreen">
                 <section className="modal-content">
                     <section className="modal-body">
+                        <Carousel id={model + " Carousel"} images={product.images[0]}/>
                         <button type="button" data-bs-dismiss="modal" className="btn btn-danger">
                             Cancel
                         </button>
