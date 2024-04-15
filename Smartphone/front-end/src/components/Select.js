e3ae5ec4b15155eb77e540a896ae7cedbf1e2356
hqiao8 <qiaohaipeng014@gmail.com>
2024-04-14 20:50:56 -0600

    Set up the Select

---
+++ Smartphone/front-end/src/components/Select.js
@@ -0,0 +1,22 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+const Select = props => {
+    const id = props.id.replace(/\s+/g, "-");
+    const options = props.options;
+    return (
+        <section className="m-1 form-floating">
+            <select id={id} onchange={event => props.setIndex(options.indexOf(event.target.value))} className="form-select" name={props.name}>
+                {options.map(option => <option value={option}>{option}</option>)}
+            </select>
+            <label for={id} className="form-label">
+                {props.label}
+                <output name="">
+                    {props.ouput}
+                </output>
+            </label>
+        </section>
+    );
+};
+
+export default Select;
