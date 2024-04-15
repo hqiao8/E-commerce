451e82595d98eddc9cf2a71160dd1ce5782f2f11
hqiao8 <qiaohaipeng014@gmail.com>
2024-04-14 20:55:41 -0600

    Update the Card for Select additions

--- Smartphone/front-end/src/components/Card.js
+++ Smartphone/front-end/src/components/Card.js
@@ -1,11 +1,16 @@
+import {useState} from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import Carousel from "./Carousel";
+import Select from "./Select"
 
 const Card = props => {
     const product = props.product;
     const model = product.model;
     const id = model.replace(/\s+/g, "-");
+    const [i, setI] = useState(0);
+    const [j, setJ] = useState(0);
+    let price = product.prices[j];
     return [
         <figure className="card w-50">
             <img src={product.image} alt={model} className="card-img-top img-thumbnail w-100"/>
@@ -18,15 +23,20 @@ const Card = props => {
                 </button>
             </figcaption>
         </figure>,
-        <section className="modal fade" id={id}>
+        <section id={id} className="modal fade">
             <section className="modal-dialog modal-fullscreen">
                 <section className="modal-content">
-                    <section className="modal-body">
-                        <Carousel id={model + " Carousel"} images={product.images[0]}/>
-                        <button type="button" data-bs-dismiss="modal" className="btn btn-danger">
+                    <form className="modal-body">
+                        <Carousel images={product.images[i]} id={model + " Carousel"}/>
+                        <Select options={product.colours} label="Colours" output="" id={model + " Colours"} setIndex={setI} name=""/>
+                        <Select options={product.storages} label={"Storages-$" + price} output={price} id={model + " Storages"} setIndex={setJ} name=""/>
+                        <button type="submit" className="m-1 w-25 btn btn-primary">
+                            Add
+                        </button>
+                        <button type="button" data-bs-dismiss="modal" className="m-1 w-25 btn btn-danger">
                             Cancel
                         </button>
-                    </section>
+                    </form>
                 </section>
             </section>
         </section>
