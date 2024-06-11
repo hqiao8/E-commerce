467de05e2908060f4e9258ed5ab34be96156c71e
hqiao8 <qiaohaipeng014@gmail.com>
2024-06-10 21:16:39 -0600

    Set up the Footer

---
+++ Smartphone/front-end/src/components/Footer.js
@@ -0,0 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFacebookF, faLinkedinIn, faXTwitter, faYoutube} from "@fortawesome/free-brands-svg-icons";
+
+const Footer = () => (
+    <footer className="bg-info p-3">
+        <button type="button" className="m-4 btn btn-outline-primary rounded-pill">
+            <FontAwesomeIcon icon={faFacebookF}/>
+        </button>
+        <button type="button" className="m-4 btn btn-outline-primary rounded-pill">
+            <FontAwesomeIcon icon={faLinkedinIn}/>
+        </button>
+        <button type="button" className="m-4 btn btn-outline-primary rounded-pill">
+            <FontAwesomeIcon icon={faXTwitter}/>
+        </button>
+        <button type="button" className="m-4 btn btn-outline-primary rounded-pill">
+            <FontAwesomeIcon icon={faYoutube}/>
+        </button>
+    </footer>
+);
+
+export default Footer;
