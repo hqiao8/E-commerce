ff2cad157e5dfafdbfb9afc77a57fd4c8d0ef964
hqiao8 <qiaohaipeng014@gmail.com>
2024-06-17 16:14:11 -0600

    Update the App

--- Smartphone/front-end/src/App.js
+++ Smartphone/front-end/src/App.js
@@ -1,7 +1,16 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const App = () => (
-  <Home/>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
